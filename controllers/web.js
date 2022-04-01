77470733e136e370ba349e70ba1009ca9c470b36
Jhonatan <yamidjhonatan@gmail.com>
2022-03-31 23:34:43 -0500

    Se corrige ruta svg en controller

--- controllers/web.js
+++ controllers/web.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const getQr = (req, res) => {
     res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    fs.createReadStream(`${__dirname}/../mediaSend/qr-code.svg`).pipe(res);
+    fs.createReadStream(`./mediaSend/qr-code.svg`).pipe(res);
 }
 
 module.exports = { getQr }
